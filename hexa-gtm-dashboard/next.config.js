a94899758a30e6d43dcbdff47dd00744e5e2c6ef
quentinlegall24-hub <quentin.legall24@gmail.com>
2026-03-24 09:58:38 +0100

    Add outputFileTracingIncludes to next.config.js

--- hexa-gtm-dashboard/next.config.js
+++ hexa-gtm-dashboard/next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    outputFileTracingIncludes: {
+          '/': ['./public/data.json'],
+          '/api/reports': ['./public/data.json'],
+    },
+}
 module.exports = nextConfig
